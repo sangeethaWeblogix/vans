d147e2cab19dc0db514318457c26fd27055b252e
Pavi-weblogix <pavi@marketplacenetwork.com.au>
2026-04-03 18:00:42 +0530

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,18 +17,18 @@ import ThemeRegistry from './components/ThemeRegistry';
 import NavigationHistory from "@/components/NavigationHistory";
 import { BannerProvider } from "@/components/BannerHandler";
 
-  
+
   export const metadata: Metadata = {
     title: {
-      default: "Motorhomes For Sale – Australia’s Marketplace for New & Used Caravans",
+      default: "Vans For Sale – Australia’s Marketplace for New & UsedCampervans",
       template: "%s ",
     },
     description:
-      "Browse new & used motorhomes for saleacross Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity",
+      "Browse new & used Vans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity",
     icons: { icon: "/favicon.ico" },
-    // robots: "index, follow",
+    robots: "index, follow",
     verification: {
-        // // google: "6tT6MT6AJgGromLaqvdnyyDQouJXq0VHS-7HC194xEo", // ✅ this auto generates <meta name="google-site-verification" />
+      // google: "6tT6MT6AJgGromLaqvdnyyDQouJXq0VHS-7HC194xEo", // ✅ this auto generates <meta name="google-site-verification" />
     },
     alternates: {
       canonical: "https://www.caravansforsale.com.au",
